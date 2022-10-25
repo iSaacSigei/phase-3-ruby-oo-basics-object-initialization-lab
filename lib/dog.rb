05eeb55945cc2a5108a50407cd2217f50e36ad33
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-25 12:45:09 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,12 +10,3 @@ end
 
 new_dog=Dog.new "Junnie", "Bull-Dog"
 
-
-#initialize Person
-class Person
-    def initialize name
-        @name=name    
-    end
-end
-
-this_person= Person.new "Junnie"
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+
+#initialize Person
+class Person
+    def initialize name
+        @name=name    
+    end
+end
+
+this_person= Person.new "Junnie"
